74ea429bd845fd40ada4e372dde9c9be429766fa
Fahad Mukunzi <fahadm507@gmail.com>
2014-06-08 18:38:15 -0400

    stuck on the last error for hours

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -5,20 +5,18 @@ require 'pry'
 class BoundingArea
   attr_reader :top, :bottom, :left, :right
   def initialize(rects)
-    @rects = rects
-    @rects.each do |point|
-      @left = point.left
-      @right = point.right
-      @top = point.top
-      @bottom = point.bottom
+    rects.each do |rect|
+      @left = rect.left
+      @right = rect.right
+      @top = rect.top
+      @bottom = rect.bottom
     end
   end
 
   def contains_point?(x,y)
-
     if x == 0 && y == 0
       false
-    elsif (x >= @left) && (x <= @right) && (y >= @bottom) && (y <= @top)
+    elsif ((x >= @left) && (x <= @right) && (y >= @bottom) && (y <= @top))
       true
     else
       false
